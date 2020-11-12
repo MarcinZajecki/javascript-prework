158b11fd5b72e0f68ea4f92de46380665a6cfd6a
MarcinZajecki <mar.zaj327@gmail.com>
2020-11-12 15:08:03 +0100

    First excercise

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+// Variables
 let computerMove = 'kamień';
 let playerMove = 'papier';
+let randomFraction = Math.random ();
+let calculation = randomFraction * 3 + 1; 
+let roundNumber = Math.floor (calculation);
 
-printMessage ('Zagrałem ' + computerMove + ' Jeśli twój ruch to' + playerMove + ' to wygrywasz');
+
+printMessage ('Zagrałem ' + computerMove + '.' + ' Jeśli twój ruch to ' + playerMove + ' to wygrywasz.');
+
+printMessage ('Wylosowany ułamek to ' + randomFraction);
+
+printMessage ('Ułamek pomnożony przez 3 i powiększony o 1 to ' + calculation);
+
+printMessage ('Liczba po zaokrągleniu w dół to ' + roundNumber);
+
+
+// od 11 do 19
+
+// variables
+let gameResult = Math.floor (Math.random() * 9 + 11);
+
+printMessage ('Wylosowana większa liczba to ' + gameResult);
